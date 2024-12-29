bfaf01803a430dcb7c5c7f827c890cf33d44c269
laizu01993 <laizuaktar6@gmail.com>
2024-12-30 02:42:28 +0600

    added shared section title for different section

--- src/Pages/Home/Category/Category.jsx
+++ src/Pages/Home/Category/Category.jsx
@@ -8,10 +8,18 @@ import slide2 from '../../../assets/home/slide2.jpg'
 import slide3 from '../../../assets/home/slide3.jpg'
 import slide4 from '../../../assets/home/slide4.jpg'
 import slide5 from '../../../assets/home/slide5.jpg'
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
 
 
 const Category = () => {
     return (
+        
+        <div>
+            <SectionTitle
+            subHeading={"From 11:00am to 10:00pm"}
+            heading={"ORDER ONLINE"}
+            >
+            </SectionTitle>
         <Swiper
             slidesPerView={4}
             spaceBetween={30}
@@ -20,8 +28,9 @@ const Category = () => {
                 clickable: true,
             }}
             modules={[Pagination]}
-            className="mySwiper mb-24"
+            className="mySwiper mb-24 mt-10"
         >
+            
             <SwiperSlide>
                 <img src={slide1} />
                 <h3 className='text-3xl text-center -mt-12 uppercase text-white '>Salads</h3>
@@ -43,6 +52,7 @@ const Category = () => {
                 <h3 className='text-3xl text-center uppercase -mt-12 text-white'>Salads</h3>
             </SwiperSlide>
         </Swiper>
+        </div>
     );
 };
 
