eadba7f042f98345d782f3bed50d6ea1655b64a5
laizu01993 <laizuaktar6@gmail.com>
2024-12-29 23:03:08 +0600

    created a simple responsive top banner

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,7 +2,8 @@ import {
     createBrowserRouter,
   } from "react-router-dom";
 import MainLayout from "../Layout/MainLayout";
-import Home from "../Pages/Home";
+import Home from "../Pages/Home/Home";
+
 
 
   export const router = createBrowserRouter([
