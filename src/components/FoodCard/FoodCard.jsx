e6515e2f321dd8fce11dbe918b821a2a9a06db40
laizu01993 <laizuaktar6@gmail.com>
2025-10-23 19:56:52 +0600

    enforce user to login page before add to the cart

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -1,10 +1,36 @@
+import { useContext } from "react";
+import { AuthContext } from "../../providers/AuthProvider";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
 
-const FoodCard = ({item}) => {
+const FoodCard = ({ item }) => {
 
     const { name, recipe, image, price } = item;
 
+    const { user } = useContext(AuthContext);
+
+    const navigate = useNavigate();
+
     const handleAddToCart = food => {
-        console.log(food);
+        if (user && user.email) {
+            // send 
+        }
+        else {
+            Swal.fire({
+                title: "You are not logged in",
+                text: "Please login to add to the cart",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Yes, login!"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                //    send the user to the login page
+                navigate('/login')
+                }
+            });
+        }
     }
 
     return (
@@ -18,7 +44,7 @@ const FoodCard = ({item}) => {
                 <h2 className="card-title">{name}</h2>
                 <p>{recipe}</p>
                 <div className="card-actions">
-                    <button onClick={() =>handleAddToCart(item)} className="btn uppercase btn-outline border-0 border-b-4 mt-4 text-yellow-500
+                    <button onClick={() => handleAddToCart(item)} className="btn uppercase btn-outline border-0 border-b-4 mt-4 text-yellow-500
                     bg-gray-100 hover:bg-black">Add to cart</button>
                 </div>
             </div>
