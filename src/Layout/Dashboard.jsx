e5ddd29e8cab417f501a56662f5f47cbc92a441c
laizu01993 <laizuaktar6@gmail.com>
2025-10-26 17:40:59 +0600

    created cart data from custom hook and calculate total price

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,7 +1,7 @@
 
-import { FaCalendarAlt, FaListAlt, FaShoppingCart } from "react-icons/fa";
-import { IoHomeSharp, IoWallet } from "react-icons/io5";
-import { MdRateReview } from "react-icons/md";
+import { FaCalendarAlt, FaListAlt, FaShoppingBag, FaShoppingCart } from "react-icons/fa";
+import { IoHomeSharp, IoMenu, IoWallet } from "react-icons/io5";
+import { MdEmail, MdRateReview } from "react-icons/md";
 import { NavLink, Outlet } from "react-router-dom";
 
 const Dashboard = () => {
@@ -9,26 +9,60 @@ const Dashboard = () => {
         <div className="flex">
             {/* Dashboard side bar */}
             <div className="w-64 min-h-screen bg-amber-500">
-                <ul className="menu p-4 ">
+                <ul className="menu p-4 space-y-3">
                     <li>
-                        <NavLink className="text-xl " to="/dashboard/userHome"> <IoHomeSharp className="text-xl" />User Home</NavLink>
+                        <NavLink className={({ isActive }) =>
+                            isActive
+                                ? "text-white text-xl"
+                                : "text-black hover:text-white hover:bg-amber-400 text-xl"
+                        } to="/dashboard/userHome"> <IoHomeSharp className="text-xl" />User Home</NavLink>
                     </li>
                     <li>
-                        <NavLink className="text-xl" to="/dashboard/reservation"> <FaCalendarAlt className="text-xl" />Reservation</NavLink>
+                        <NavLink className={({ isActive }) =>
+                            isActive
+                                ? "text-white text-xl"
+                                : "text-black hover:text-white hover:bg-amber-400 text-xl"} to="/dashboard/reservation"> <FaCalendarAlt className="text-xl" />Reservation</NavLink>
                     </li>
                     <li>
-                        <NavLink className="text-xl " to="/dashboard/cart"> <IoWallet className="text-xl" />Payment History</NavLink>
+                        <NavLink className={({ isActive }) =>
+                            isActive
+                                ? "text-white text-xl"
+                                : "text-black hover:text-white hover:bg-amber-400 text-xl"} to="/dashboard/payment"> <IoWallet className="text-xl" />Payment History</NavLink>
                     </li>
                     <li>
-                        <NavLink className="text-xl " to="/dashboard/cart"> <FaShoppingCart className="text-xl" />My Cart</NavLink>
+                        <NavLink className={({ isActive }) =>
+                            isActive
+                                ? "text-white text-xl"
+                                : "text-black hover:text-white hover:bg-amber-400 text-xl"} to="/dashboard/cart"> <FaShoppingCart className="text-xl" />My Cart</NavLink>
                     </li>
                     <li>
-                        <NavLink className="text-xl " to="/dashboard/review"> <MdRateReview className="text-xl" />Add Review</NavLink>
+                        <NavLink className={({ isActive }) =>
+                            isActive
+                                ? "text-white text-xl"
+                                : "text-black hover:text-white hover:bg-amber-400 text-xl"} to="/dashboard/review"> <MdRateReview className="text-xl" />Add Review</NavLink>
                     </li>
                     <li>
-                        <NavLink className="text-xl " to="/dashboard/bookings"> <FaListAlt className="text-xl" />My Booking</NavLink>
+                        <NavLink className={({ isActive }) =>
+                            isActive
+                                ? "text-white text-xl"
+                                : "text-black hover:text-white hover:bg-amber-400 text-xl"} to="/dashboard/bookings"> <FaListAlt className="text-xl" />My Booking</NavLink>
+                    </li>
+                    <div className="divider "></div>
+
+                    <li>
+                        <NavLink className="text-black hover:text-white hover:bg-amber-400 text-xl" to="/"> <IoHomeSharp className="text-xl" />Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink className="text-black hover:text-white hover:bg-amber-400 text-xl" to="/menu"> <IoMenu className="text-xl" />Menu</NavLink>
+                    </li>
+                    <li>
+                        <NavLink className="text-black hover:text-white hover:bg-amber-400 text-xl" to="/order/salad"> <FaShoppingBag className="text-xl" />Shop</NavLink>
+                    </li>
+                    <li>
+                        <NavLink className="text-black hover:text-white hover:bg-amber-400 text-xl" to="/contact"> <MdEmail className="text-xl" />Contact</NavLink>
                     </li>
                 </ul>
+
             </div>
             {/* Dashboard content */}
             <div className="flex-1">
--- src/pages/Dashboard/Cart/Cart.jsx
+++ src/pages/Dashboard/Cart/Cart.jsx
@@ -1,7 +1,71 @@
+import { RiDeleteBin6Line } from "react-icons/ri";
+import useCart from "../../../hooks/useCart";
 
-const Cart = () =>{
+const Cart = () => {
+
+    const [cart] = useCart();
+
+    // Calculation of total price
+    const totalPrice = cart.reduce((total, item) => total + item.price, 0)
+
     return (
-        <div>My Cart</div>
+        <div>
+            <div className="flex items-center justify-evenly">
+                <h2 className="text-4xl">Total Orders: {cart.length}</h2>
+                <h2 className="text-4xl">Total Price: {totalPrice}</h2>
+                <button className="btn bg-amber-500 btn-md hover:bg-amber-400 text-white">Pay</button>
+            </div>
+            {/* Table */}
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>
+                               # 
+                            </th>
+                            <th>Image</th>
+                            <th>Name</th>
+                            <th>Price</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            cart.map(item => <tr key={item._id}>
+                            <th>
+                                
+                            </th>
+                            <td>
+                                
+                                    <div className="avatar">
+                                        <div className="mask mask-squircle h-12 w-12">
+                                            <img
+                                                src={item.image}
+                                                alt="" />
+                                        </div>
+                                    </div>
+                                    </td>
+                                    <td>
+                                    <div>
+                                        <h2 className="font-bold">{item.name}</h2>
+                                    </div>
+                                </td>
+                            <td>
+<div>
+                                        <h2 className="font-bold">{item.price}</h2>
+                                    </div>
+                            </td>
+                           
+                            <th>
+                                <button className="btn btn-sm bg-red-500 text-white text-lg hover:bg-red-400"><RiDeleteBin6Line /></button>
+                            </th>
+                        </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
+        </div>
     );
 };
 
