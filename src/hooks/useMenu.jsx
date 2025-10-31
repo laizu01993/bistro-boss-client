0667c41b2b77c28769d1de62d49a40669265a520
laizu01993 <laizuaktar6@gmail.com>
2025-10-31 21:04:17 +0600

    added deleted function successfully in ManageItems page

--- src/hooks/useMenu.jsx
+++ src/hooks/useMenu.jsx
@@ -1,20 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
+import useAxiosPublic from "./useAxiosPublic";
 
 const useMenu = () => {
-    const [menu, setMenu] = useState([]);
 
-    const [loading, setLoading] = useState(true);
+    const axiosPublic = useAxiosPublic();
+    
 
-    useEffect(() => {
-        fetch('http://localhost:5000/menu')
-            .then(res => res.json())
-            .then(data => {
-                setMenu(data)
-                setLoading(false);
-            });
-    }, [])
+    const {refetch, data: menu = [], isPending: loading} = useQuery({
+        queryKey: ['menu'],
+        queryFn: async () =>{
+            const res = await axiosPublic.get('/menu');
+            return res.data;
+        }
+    })
 
-    return [menu, loading]
+    return [menu, loading, refetch]
 }
 
 export default useMenu;
--- src/pages/Dashboard/ManageItems/ManageItems.jsx
+++ src/pages/Dashboard/ManageItems/ManageItems.jsx
@@ -2,11 +2,44 @@ import { FiEdit } from "react-icons/fi";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import useMenu from "../../../hooks/useMenu";
 import { RiDeleteBin6Line } from "react-icons/ri";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 
 const ManageItems = () => {
 
-    const [menu, loading] = useMenu();
+    const [menu, loading, refetch] = useMenu();
+
+    const axiosSecure = useAxiosSecure();
+
+    const handleDeleteItem = item => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then( async (result) => {
+            if (result.isConfirmed) {
+
+                const res = await axiosSecure.delete(`/menu/${item._id}`)
+                    .then( res => {
+                        if (res.data.deletedCount > 0) {
+                            refetch();
+                            Swal.fire({
+                                position: "top-end",
+                                title: `${item.name} has been deleted!`,
+                                showConfirmButton: false,
+                                timer: 1500,
+                                icon: "success"
+                            });
+                        }
+                    })
+            }
+        });
+    }
 
     return (
         <div className="p-6">
@@ -46,10 +79,10 @@ const ManageItems = () => {
                                 <td>{item.name}</td>
                                 <td>{item.price}</td>
                                 <td>
-                                    <button className="btn bg-amber-500 text-white"><FiEdit /></button>
+                                    <button className="btn bg-amber-500 hover:bg-amber-400 text-white"><FiEdit /></button>
                                 </td>
                                 <td>
-                                    <button className="btn btn-sm bg-red-500 text-white text-lg hover:bg-red-400"><RiDeleteBin6Line /></button>
+                                    <button onClick={() => handleDeleteItem(item)} className="btn btn-sm bg-red-500 text-white text-lg hover:bg-red-400"><RiDeleteBin6Line /></button>
                                 </td>
                             </tr>)
                         }
