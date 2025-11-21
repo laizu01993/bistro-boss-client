f94fdb4e7893671612a8d62fc76c2ad4ddcd3cc7
laizu01993 <laizuaktar6@gmail.com>
2025-11-21 23:34:31 +0600

    order food related some issue has been fixed

--- src/hooks/useMenu.jsx
+++ src/hooks/useMenu.jsx
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { useEffect, useState } from "react";
+
 import useAxiosPublic from "./useAxiosPublic";
 
 const useMenu = () => {
--- src/pages/Order/Order/Order.jsx
+++ src/pages/Order/Order/Order.jsx
@@ -20,7 +20,13 @@ const Order = () => {
 
      const [tabIndex, setTabIndex] = useState(initialIndex);
 
-    const [menu] = useMenu();
+    const [menu, loading] = useMenu();
+
+    if(loading) 
+        return <div className="p-8">
+                <Skeleton height={40} width={200} />
+                <Skeleton count={5} />
+              </div>
 
     const dessert = menu.filter(item => item.category === "dessert")
     const pizza = menu.filter(item => item.category === "pizza")
